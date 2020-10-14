af4f22755327c57c814d6b2cb12b05574c176380
Andre Luiz <andreluizkanaan222@gamil.com>
2020-10-14 18:19:41 -0300

    Video component created

--- src/components/Recommended/index.jsx
+++ src/components/Recommended/index.jsx
@@ -1,9 +1,119 @@
 import React from "react";
 
+import VideoCard from '../VideoCard';
+
 import "./Recommended.css";
 
 function Recommended() {
-  return <div className="recommended-videos" />;
+  return (
+    <div className="recommended-videos">
+      <h2>Recomendados</h2>
+      <div className="recommended-videos-container">
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+<VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+
+        <VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+
+<VideoCard
+          title="Neymar Gols Lendários Pela Seleção"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyDFDUoPwrP5eP21iBi0cnEMlRS_uMYbRMAzHYYNw=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="MNcompsJR"
+          image="https://i.ytimg.com/an_webp/NwDUwM7oLko/mqdefault_6s.webp?du=3000&sqp=CJrFnfwF&rs=AOn4CLAdfdQ6DMb7TPHU-wK2tWHodo2Hww"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Recommended;
---
+++ src/components/VideoCard/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Avatar from "@material-ui/core/Avatar";
+
+import './VideoCard.css';
+
+function VideoCard({ 
+  image, 
+  title, 
+  channel,
+  views, 
+  timestamp, 
+  channelImage
+}) {
+  return (
+    <div className='videoCard'>
+      <img className='videoCard-image' src={image} alt=""/>
+      <div className="videoCard-info">
+        <Avatar 
+          className='videoCard-avatar' 
+          alt={channel} 
+          src={channelImage} 
+        />
+        <div className="videoCard-text">
+          <h4>{title}</h4>
+          <p>{channel}</p>
+          <p>
+            {views}  {timestamp}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoCard;
